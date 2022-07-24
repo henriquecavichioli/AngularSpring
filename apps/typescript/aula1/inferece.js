799ff17c823528e16a05d5a554e1fecb6e84a8c3
Henrique Cavichioli <henriqueemann@gmail.com>
2022-07-24 19:49:13 -0300

    inicio

---
+++ apps/typescript/aula1/inferece.js
@@ -0,0 +1,5 @@
+function showLog(message) {
+    console.log(message);
+}
+showLog("Hello world! ");
+showLog(77);
---
+++ apps/typescript/aula1/inferece.ts
@@ -0,0 +1,16 @@
+function showLog(message: any) {
+    console.log(message);
+}
+
+showLog("Hello world! ");
+showLog(77); 
+/* 
+string
+number
+boolean
+arrays
+Classe
+*/
+let test: Array<string> = ['Hello', 'Teste'];
+let isEnabled: boolean = true;
+let test2: [string] = ['Hello', 'Test'];
